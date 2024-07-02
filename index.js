12a5a9ee53d03d7639ecd429f563286599fca142
Khagendra Kumar <itskhagendra@hotmail.com>
2024-07-02 10:34:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,28 +8,28 @@ const Data = [
     {
         Name:"Big Buck Bunny",
         Image:"https://img.freepik.com/free-photo/majestic-mountain-peak-tranquil-winter-landscape-generated-by-ai_188544-15662.jpg",
-        Video:"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+        Video:"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+        is360:false
 
     },
     {
         Name:"Elephant Dream",
         Image:"https://t4.ftcdn.net/jpg/05/68/06/93/360_F_568069372_X1vDTdjtvKbVLOEegIPZZrIorrHlQc7R.jpg",
-        Video:"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
+        Video:"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+        is360:false
 
     },
     {
-        Name:"Elephant Dream",
+        Name:"Bigger Blazes",
         Image:"https://img.freepik.com/free-photo/cool-geometric-triangular-figure-neon-laser-light-great-backgrounds-wallpapers_181624-9331.jpg",
-        Video:"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4"
+        Video:"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
+        is360:false
     }   
     
 ]
 const status = {
     status:200,
     timestamp:new Date()
-}
-function randomIntFromInterval(min, max) { // min and max included 
-    return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 // app.get("/image", (req, res) => {
@@ -52,3 +52,4 @@ app.get("/v1/data",(req,res)=>{
 app.listen(8000, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
 })
+// exports.api = functions.https.onRequest(app);
